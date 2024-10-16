fc1ab8b25b47312232e5c2a1978a0a68e8a1616d
Mark Sweitzer <msweitzer1987@gmail.com>
2024-10-16 00:38:34 -0400

    saniton root dir

--- frontend/src/app/api/send/route.ts
+++ frontend/src/app/api/send/route.ts
@@ -1,19 +1,28 @@
 import { EmailTemplate } from '../../components/EmailTemplate';
 import { Resend } from 'resend';
 import * as React from 'react';
+import { NextRequest } from 'next/server';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
+type Payload = {
+  firstName: string,
+  lastName: string
+}
 
-export  async function POST() {
+
+export  async function POST(req : NextRequest) {
+  const formData: Payload = await req.json();;
   try {
     const { data, error } = await resend.emails.send({
       from: 'Acme <onboarding@resend.dev>',
-      to: ['delivered@resend.dev'],
-      subject: "Hello world",
-      react: EmailTemplate({ firstName: "John" }) as React.ReactElement,
+      to: [`${process.env.RESEND_API_DELIVERY_EMAIL}`],
+      subject: "Contact Submission",
+      react: EmailTemplate({ firstName: formData.firstName, lastName : formData.lastName
+      }) as React.ReactElement,
     });
 
     if (error) {
+      console.log(error.message);
       return Response.json({ error }, { status: 500 });
     }
 
---
+++ frontend/src/app/schemas/index.ts
@@ -0,0 +1,7 @@
+
+import { pomskyType } from './pomskyType'
+import {postType} from './postType.js';
+import {litterType} from './litterType.js';
+import {locationType} from './locationType.js';
+import { pageType } from './pageType';
+export const schemaTypes = [pageType, postType, litterType, pomskyType, locationType]
