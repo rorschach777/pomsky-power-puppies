bc6505c0c4404ba9b2e205baf836944643494ab4
Mark Sweitzer <msweitzer1987@gmail.com>
2024-10-23 10:53:57 -0400

    SEO alt text on images and social media icons

--- frontend/src/app/components/Inclusions.tsx
+++ frontend/src/app/components/Inclusions.tsx
@@ -7,7 +7,7 @@ const Inclusions = () => {
                 <h2>Puppy Inclusions</h2>
                 <div className="ppp-flex-container ">
                     <div className="ppp-inclusion">
-                        <img src="/images/icons/icon-1.svg" /> 
+                        <img alt="EMS | Neurological Stimulation" src="/images/icons/icon-1.svg" /> 
                         <h4>Neuroglial Stimulation</h4>
                         <p>
                         Properly done ENS provides the puppies with improved growth and development in their immune and cardiovascular systems, stress tolerance, and so much more. 
@@ -17,7 +17,7 @@ const Inclusions = () => {
                         </Button>
                     </div>
                     <div className="ppp-inclusion">
-                        <img src="/images/icons/icon-2.svg"  /> 
+                        <img alt="We all need good puppy manners!" src="/images/icons/icon-2.svg"  /> 
                         <h4>Puppy Manners</h4>
                         <p>
                         Once our puppies begin to romp and play, they are introduced to puppy manners, a form of training using positive reinforcement and behavior redirecting. 
@@ -27,7 +27,7 @@ const Inclusions = () => {
                         </Button>
                     </div>
                     <div className="ppp-inclusion">
-                        <img src="/images/icons/icon-3.svg"  /> 
+                        <img alt="Rest assured in your purchase with 30-Day Insurance" src="/images/icons/icon-3.svg"  /> 
                         <h4>30-Day Pet Insurance</h4>
                         <p>
                             Trupanian sponsors each puppy by providing each puppy with 30 days free pet insurance, excluding California and NY, as their state laws forbid this.
@@ -37,10 +37,10 @@ const Inclusions = () => {
                         </Button>
                     </div>
                     <div className="ppp-inclusion">
-                        <img  src="/images/icons/icon-4.svg"  /> 
+                        <img  alt="We go above and beyond to keep our puppy's healthy" src="/images/icons/icon-4.svg"  /> 
                         <h4>Health</h4>
                         <p>
-                        Trupanian sponsors each puppy by providing each puppy with 30 days free pet insurance, excluding California and NY, as their state laws forbid this.
+                        Each puppy's contract includes our health guarantee. Still, we go above and beyond with our puppy's health by providing each puppy with a thorough vet health certificate. Any puppy traveling with our flight nanny receives a flight certificate. We raise our puppies in a clean, healthy home! Their health reflects this.
                         </p>
                         <Button variant="bordered" color="secondary">
                             <Link href="tel:6108000012" aria-description="Get in touch with us today by calling or texting" >Text or Call Today</Link>
