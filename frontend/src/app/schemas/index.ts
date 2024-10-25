9101574beb75137f79f127831ebf53f4ee800a5d
Mark Sweitzer <msweitzer1987@gmail.com>
2024-10-24 21:51:45 -0400

    eye color options set up at reference type

--- frontend/src/app/schemas/index.ts
+++ frontend/src/app/schemas/index.ts
@@ -5,5 +5,5 @@ import {litterType} from './litterType';
 import {locationType} from './locationType';
 import { pageType } from './pageType';
 import { eyeType } from './eyeType';
-import { coatType } from './coatType';
+
 export const schemaTypes = [pageType, postType, litterType, pomskyType, locationType, eyeType];
