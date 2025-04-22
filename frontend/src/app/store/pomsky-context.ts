afdc6f24dbfae54aa74a48ae0ba846b52b9e64d9
Mark Sweitzer <msweitzer1987@gmail.com>
2025-04-22 10:33:35 -0400

    first commit

--- frontend/src/app/store/pomsky-context.ts
+++ frontend/src/app/store/pomsky-context.ts
@@ -1,40 +1,40 @@
-import React from 'react';
-import { IData } from '../interfaces/interfaces';
+// import React from 'react';
+// import { IData } from '../interfaces/interfaces';
 
 
-//     _id,
-//     description, 
-//     litterParents,
-//     publishedAt,
-//     published,
-//     location[]->{
-//       locationName
-//     },
-//     soldOut,
-//     published,
-//     litterName,
-//     puppies[]->{
-//       description,
-//       currentlyAvailable,
-//       published,
-//       pomskyName,
-//       weight,
-//       female,
-//       image{
-//         asset->
-//       },
-//       backgroundImage{
-//         asset->
-//       },
-//       eyeColor-> {
-//       color
-//       },
-//       isPuppy,
-//       showPrice,
-//       price
-//     }
-const PomskyContext  = React.createContext({
-    data : {} as IData
-});
+// //     _id,
+// //     description, 
+// //     litterParents,
+// //     publishedAt,
+// //     published,
+// //     location[]->{
+// //       locationName
+// //     },
+// //     soldOut,
+// //     published,
+// //     litterName,
+// //     puppies[]->{
+// //       description,
+// //       currentlyAvailable,
+// //       published,
+// //       pomskyName,
+// //       weight,
+// //       female,
+// //       image{
+// //         asset->
+// //       },
+// //       backgroundImage{
+// //         asset->
+// //       },
+// //       eyeColor-> {
+// //       color
+// //       },
+// //       isPuppy,
+// //       showPrice,
+// //       price
+// //     }
+// // const PomskyContext  = React.createContext({
+// //     data : {} as IData
+// // });
 
-export default PomskyContext;
+// export default PomskyContext;
