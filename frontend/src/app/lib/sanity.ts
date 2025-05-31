007902d5ee69560b12b2f21eedbb2ebae94d21f3
Mark Sweitzer <msweitzer1987@gmail.com>
2025-05-30 23:08:52 -0400

    puppy card to use SanityImage / dynamic sizing

--- frontend/src/app/lib/sanity.ts
+++ frontend/src/app/lib/sanity.ts
@@ -1,9 +1,10 @@
 import { createClient } from 'next-sanity'
 import imageUrlBuilder from '@sanity/image-url'
+import {dataset, projectId} from '../../environment'
 
 export const sanityClient = createClient({
-  projectId: 'your_project_id',     // Replace with your actual Sanity project ID
-  dataset: 'production',            // Replace with your dataset
+  projectId: projectId,     // Replace with your actual Sanity project ID
+  dataset: dataset,            // Replace with your dataset
   useCdn: true,
   apiVersion: '2023-05-30',
 })
